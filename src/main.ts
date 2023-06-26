ec5090357ccb3b1b3edee4e99680bb389c96fb2d
Sion Kang <siontama@gmail.com>
2023-06-26 19:46:45 +0900

    chore: swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import expressBasicAuth from 'express-basic-auth';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.enableCors();
+  app.use(
+    ['/docs'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [configService.get('SWAGGER_USER')]: configService.get('SWAGGER_PWD'),
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('LifePoop Api Server')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'lifepoop API',
+    customCss: '.swagger-ui .topbar { display: none }',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
