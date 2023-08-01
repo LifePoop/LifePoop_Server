7f0f16fec838a5f0d4895549e6334225c06ca812
Sion Kang <siontama@gmail.com>
2023-08-01 20:17:26 +0900

    fix: 새롭게 만들어준 dto에 맞게 수정

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,9 +1,15 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostService } from './post.service';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Auth } from 'src/auth/decorator/auth.decorator';
-import { CreatePostRequestDto } from './dto/request/creat-post-reuqest.dto';
+import { CreatePostRequestDto } from './dto/request/create-post-reuqest.dto';
 import { Post as PostEntity } from '@app/entity/post/post.entity';
+import { PostResponseDto } from './dto/response/post-response.dto';
 
 @ApiTags('post')
 @Controller('post')
@@ -13,11 +19,14 @@ export class PostController {
   @Auth()
   @Post()
   @ApiOperation({ summary: '변기록 생성' })
-  @ApiCreatedResponse({ description: '업로드 된 변기록', type: PostEntity })
+  @ApiCreatedResponse({
+    description: '업로드 된 변기록',
+    type: PostResponseDto,
+  })
   async create(
     @Body()
     { writerId, isGood, color, size, shape, date }: CreatePostRequestDto,
-  ): Promise<PostEntity> {
+  ): Promise<PostResponseDto> {
     return await this.postService.create(
       writerId,
       isGood,
@@ -31,16 +40,22 @@ export class PostController {
   @Auth()
   @Get(':userId')
   @ApiOperation({ summary: '특정 유저의 변기록 조회' })
-  @ApiCreatedResponse({ description: '특정 유저의 변기록' })
-  async get(@Param('userId') userId: number): Promise<PostEntity[]> {
+  @ApiOkResponse({ description: '특정 유저의 변기록', type: [PostResponseDto] })
+  async getByUser(@Param('userId') userId: number): Promise<PostResponseDto[]> {
     return await this.postService.findByUser(userId);
   }
 
   @Auth()
   @Get('/:userId/:date')
-  @ApiOperation({ summary: '나의 오늘의 변기록 조회' })
-  @ApiCreatedResponse({ description: '나의 오늘의 변기록' })
-  async getToday(@Param('userId') userId: number, @Param('date') date: Date) {
+  @ApiOperation({ summary: '특정 유저의 특정 일자 변기록 조회' })
+  @ApiOkResponse({
+    description: '특정 유저의 특정 일자 변기록',
+    type: PostResponseDto,
+  })
+  async getByUserAndDate(
+    @Param('userId') userId: number,
+    @Param('date') date: Date,
+  ): Promise<PostResponseDto> {
     return await this.postService.findByUserAndDate(userId, date);
   }
 }
