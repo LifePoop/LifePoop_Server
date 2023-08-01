f07c69e893dd62432c846f3ba2480ad3d5d44ec0
Sion Kang <siontama@gmail.com>
2023-08-01 20:17:39 +0900

    feat: GET user api 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorator/auth.decorator';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from '@app/entity/user/user.entity';
+import { UserResponseDto } from './dto/response/user-response.dto';
 
+@ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Auth()
+  @Get(':userId')
+  @ApiOperation({ summary: '특정 유저 조회' })
+  @ApiOkResponse({ description: '특정 유저', type: UserResponseDto })
+  async get(@Param('userId') userId: number): Promise<UserResponseDto> {
+    return await this.userService.findById(userId);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { User } from '../../libs/entity/user/user.entity';
 
 @Injectable()
 export class UserService {
+  findById(userId: number): User {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly userRepository: UserRepository) {}
 }
