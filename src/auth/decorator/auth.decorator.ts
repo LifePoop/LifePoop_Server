0cc84811d870d6e54e97d442161c4e78ba714afd
Me1e <mele0404@naver.com>
2023-09-19 21:31:17 +0900

    feat: Auth decorator

--- src/auth/decorator/auth.decorator.ts
+++ src/auth/decorator/auth.decorator.ts
@@ -1,10 +1,16 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../utils/guards/jwt-auth.guard';
+import { JwtAccessGuard } from '../utils/guards/jwt-access.guard';
+import { JwtRefreshGuard } from '../utils/guards/jwt-refresh.guard';
 
-export function Auth() {
-  return applyDecorators(
-    UseGuards(JwtAuthGuard),
-    ApiBearerAuth('access-token'),
-  );
+export function Auth(mode: 'access' | 'refresh') {
+  switch (mode) {
+    case 'access':
+      return applyDecorators(
+        UseGuards(JwtAccessGuard),
+        ApiBearerAuth('access-token'),
+      );
+    case 'refresh':
+      return applyDecorators(UseGuards(JwtRefreshGuard));
+  }
 }
