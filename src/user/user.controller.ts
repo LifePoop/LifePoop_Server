367374bddc72d8d815ace326bdb2d7c2edf06863
Sion Kang <siontama@gmail.com>
2023-09-20 07:26:12 +0900

    feat: 친구 추가 api 추가 및 UserRequest 적용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Auth } from 'src/auth/decorator/auth.decorator';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserResponseDto } from './dto/response/user-response.dto';
 import { UpdateUserRequestDto } from './dto/request/update-user-request.dto';
+import { UserRequest } from 'src/common/decorators/user-request.decorator';
+import { UserPayload } from 'src/auth/types/jwt-payload.interface';
 
 @ApiTags('user')
 @Controller('user')
@@ -19,12 +21,12 @@ export class UserController {
   }
 
   @Auth('access')
-  @Put(':userId')
-  @ApiOperation({ summary: '특정 유저 수정' })
+  @Put()
+  @ApiOperation({ summary: '내 정보 수정' })
   @ApiOkResponse({ description: '변경된 유저 정보', type: UserResponseDto })
   async update(
     @Body() { nickname, characterColor, characterShape }: UpdateUserRequestDto,
-    @Param('userId') userId: number,
+    @UserRequest() { userId }: UserPayload,
   ): Promise<UserResponseDto> {
     return await this.userService.update(
       nickname,
@@ -35,12 +37,23 @@ export class UserController {
   }
 
   @Auth('access')
-  @Get('friendship/:userId')
-  @ApiOperation({ summary: '특정 유저의 친구 조회' })
-  @ApiOkResponse({ description: '특정 유저의 친구', type: [UserResponseDto] })
+  @Get('friendship')
+  @ApiOperation({ summary: '나의 친구 조회' })
+  @ApiOkResponse({ description: '나의 친구', type: [UserResponseDto] })
   async getFriendship(
-    @Param('userId') userId: number,
+    @UserRequest() { userId }: UserPayload,
   ): Promise<UserResponseDto[]> {
     return await this.userService.findFriendship(userId);
+  }
+
+  @Auth('access')
+  @Post('friendship/:inviteCode')
+  @ApiOperation({ summary: '친구 추가' })
+  @ApiOkResponse({ description: '친구 추가 성공' })
+  async addFriendship(
+    @Param('inviteCode') inviteCode: string,
+    @UserRequest() { userId }: UserPayload,
+  ): Promise<void> {
+    await this.userService.addFriendship(inviteCode, userId);
   }
 }
