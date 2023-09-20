e5fbbc9e5777f56d9026d9bab3da0f03f2b4f703
Sion Kang <siontama@gmail.com>
2023-09-20 10:40:14 +0900

    fix: user module에 Friendship respository 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { Friendship } from '@app/entity/friendship/friendship.entity';
+import { Repository } from 'typeorm';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, Repository<Friendship>],
   exports: [UserRepository],
 })
 export class UserModule {}
