53c9569cdfbaf53316db51209df3a8dd6043f6bb
Me1e <mele0404@naver.com>
2023-09-20 12:12:03 +0900

    feat: access guard error handling

--- src/auth/utils/guards/jwt-access.guard.ts
+++ src/auth/utils/guards/jwt-access.guard.ts
@@ -1,5 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAccessGuard extends AuthGuard('jwt-access-token') {}
+export class JwtAccessGuard extends AuthGuard('jwt-access-token') {
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    if (info !== undefined) {
+      switch (info.message) {
+        case 'No auth token':
+        case 'jwt expired':
+        case 'invalid signature':
+        case 'invalid token':
+        case 'jwt malformed':
+          throw new HttpException(info.message, 444);
+        default:
+      }
+    }
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
