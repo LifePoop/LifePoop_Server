e0472346df3cdcf6d0698fe9795cffd8b58ce3f8
Me1e <mele0404@naver.com>
2023-09-20 12:12:09 +0900

    feat: refresh guard error handling

--- src/auth/utils/guards/jwt-refresh.guard.ts
+++ src/auth/utils/guards/jwt-refresh.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token') {}
+export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token') {
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    if (info !== undefined) {
+      switch (info.message) {
+        case 'No auth token':
+        case 'jwt expired':
+        case 'invalid signature':
+        case 'invalid token':
+        case 'jwt malformed':
+          context.getResponse().clearCookie('refresh_token');
+          throw new HttpException(info.message, 445);
+        default:
+      }
+    }
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
