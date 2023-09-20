6b4c855e7765a4ee2e44f94a7451283a38bb5065
Me1e <mele0404@naver.com>
2023-09-20 12:07:20 +0900

    delete: 사용 안 하는 파일 삭제

--- src/auth/types/kakao-profile.interface.ts
+++
@@ -1,7 +0,0 @@
-// export interface KakaoProfile {
-//   id: number;
-//   username: string;
-//   displayName: string;
-//   provider: 'kakao';
-//   _json: { kakao_account: { email: string } };
-// }
