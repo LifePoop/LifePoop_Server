a8d4532d6dec07d5b52b25b088d7175c5dd7b061
Me1e <mele0404@naver.com>
2023-09-20 12:11:04 +0900

    feat: auth decorator swagger 추가

--- src/auth/decorator/auth.decorator.ts
+++ src/auth/decorator/auth.decorator.ts
@@ -1,5 +1,9 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { JwtAccessGuard } from '../utils/guards/jwt-access.guard';
 import { JwtRefreshGuard } from '../utils/guards/jwt-refresh.guard';
 
@@ -9,8 +13,22 @@ export function Auth(mode: 'access' | 'refresh') {
       return applyDecorators(
         UseGuards(JwtAccessGuard),
         ApiBearerAuth('access-token'),
+        ApiResponse({
+          status: 444,
+          description: '유효하지 않은 accessToken',
+        }),
       );
     case 'refresh':
-      return applyDecorators(UseGuards(JwtRefreshGuard));
+      return applyDecorators(
+        UseGuards(JwtRefreshGuard),
+        ApiResponse({
+          status: 445,
+          description:
+            '유효하지 않은 refresh_token, 강제 로그아웃(쿠키에 있는 refresh_token 삭제)',
+        }),
+        ApiNotFoundResponse({
+          description: 'refresh_token의 id와 일치하는 유저 없음',
+        }),
+      );
   }
 }
