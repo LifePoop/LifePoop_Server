b0ffc30065a2f6e7c17564f4cf38c89b9834c046
Me1e <mele0404@naver.com>
2023-09-27 18:09:33 +0900

    feat(api): 스토리 작성자의 오늘 변기록 수 반환

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { PostRepository } from './post.repository';
 import { StoryModule } from 'src/story/story.module';
 
 @Module({
-  imports: [StoryModule],
+  imports: [forwardRef(() => StoryModule)],
   controllers: [PostController],
   providers: [PostService, PostRepository],
+  exports: [PostRepository],
 })
 export class PostModule {}
--- src/story/story.module.ts
+++ src/story/story.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { StoryController } from './story.controller';
 import { StoryService } from './stroy.service';
 import { StoryRepository } from './stroy.repository';
 import { UserModule } from 'src/user/user.module';
 import { UserStoryViewRepository } from './user-story-view.repository';
+import { PostModule } from 'src/post/post.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, forwardRef(() => PostModule)],
   controllers: [StoryController],
   providers: [StoryService, StoryRepository, UserStoryViewRepository],
   exports: [StoryService],
