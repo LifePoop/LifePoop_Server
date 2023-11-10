3a49e602740f4ccd23ea9c25f10dbc37a32ce6de
Me1e <mele0404@naver.com>
2023-11-10 23:47:33 +0900

    delete(filter): any, http

--- src/common/filter/any-exception.filter.ts
+++
@@ -1,8 +0,0 @@
-import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
-
-@Catch()
-export class AnyExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
-    console.log(exception);
-  }
-}
